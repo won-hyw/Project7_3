9e7931d0889361882d2d006833a1b96cadee0661
won-hyw <w2019@e-mirim.hs.kr>
2021-07-26 10:34:12 +0900

    Edit MainActivity.java(Dialog_positive_button Listener) Commit

--- app/src/main/java/kr/hs/emirim/w2019/project7_3/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2019/project7_3/MainActivity.java
@@ -3,14 +3,17 @@ package kr.hs.emirim.w2019.project7_3;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView textName, textEmail;
     View dialogV;
+    EditText editName, editEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +35,15 @@ public class MainActivity extends AppCompatActivity {
             dialog.setTitle(R.string.user_input);
             dialog.setIcon(R.drawable.user);
             dialog.setView(dialogV);
-            dialog.setPositiveButton("확인", null);
+            dialog.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    editName = dialogV.findViewById(R.id.dialog_edit_name);
+                    editEmail = dialogV.findViewById(R.id.dialog_edit_email);
+                    textName.setText(editName.getText().toString());
+                    textEmail.setText(editEmail.getText().toString());
+                }
+            });
             dialog.setNegativeButton("취소", null);
             dialog.show();
         }
