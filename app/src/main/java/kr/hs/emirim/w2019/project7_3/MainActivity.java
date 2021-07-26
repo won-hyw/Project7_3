37b26fd1c43313a27657639194b34e205e97a02f
won-hyw <w2019@e-mirim.hs.kr>
2021-07-26 10:10:41 +0900

    Edit MainActivity.java(Dialog) Commit

--- app/src/main/java/kr/hs/emirim/w2019/project7_3/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2019/project7_3/MainActivity.java
@@ -1,14 +1,40 @@
 package kr.hs.emirim.w2019.project7_3;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textName, textEmail;
+    View dialogV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textName = findViewById(R.id.user_name);
+        textEmail = findViewById(R.id.user_email);
+        Button btn_input = findViewById(R.id.btn_input);
+
+        btn_input.setOnClickListener(btnListener);
     }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            dialogV = View.inflate(MainActivity.this, R.layout.dialog1, null);
+            AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+            dialog.setTitle(R.string.user_input);
+            dialog.setIcon(R.drawable.user);
+            dialog.setView(dialogV);
+            dialog.setPositiveButton("확인", null);
+            dialog.setNegativeButton("취소", null);
+            dialog.show();
+        }
+    };
 }
